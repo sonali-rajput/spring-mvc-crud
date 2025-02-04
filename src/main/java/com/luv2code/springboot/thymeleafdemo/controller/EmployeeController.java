39f0d3386cf9340ace9c895a8a1a0274e4e082e8
sonali-rajput <sonalirajput1088@gmail.com>
2025-02-04 22:04:28 +0000

    Create Postmapping for save Employee

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -39,5 +41,14 @@ public class EmployeeController {
         theModel.addAttribute("employee", theEmployee);
 
         return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+        // save the employee
+        employeeService.save(theEmployee);
+
+        // use a redirect to prevent duplicarte submissions
+        return "redirect:/employees/list";
     }
 }
